d7e5e737c490ff84968e087efaa100e65e935d5c
iwakura-X <siraevm742@gmail.com>
2025-12-09 21:51:16 +0300

    initial

--- hypr/scripts/ScreenShot.sh
+++ hypr/scripts/ScreenShot.sh
@@ -4,7 +4,7 @@
 
 # variables
 time=$(date "+%d-%b_%H-%M-%S")
-dir="$(xdg-user-dir PICTURES)/Screenshots"
+dir="/home/lain/Screenshots"
 file="Screenshot_${time}_${RANDOM}.png"
 
 iDIR="$HOME/.config/swaync/icons"
